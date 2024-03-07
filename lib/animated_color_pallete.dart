7211392d3e327b25f23d13409a1f89c102b6dc7c
Ansh Malik <thisisanshmalik@gmail.com>
2024-03-07 20:43:18 +0530

    Animaation completed using animated container

--- lib/animated_color_pallete.dart
+++ lib/animated_color_pallete.dart
@@ -10,6 +10,8 @@ class AnimatedColorPalette extends StatefulWidget {
 
 class _AnimatedColorPaletteState extends State<AnimatedColorPalette> {
   List<Color> currentPalette = generateRandomPalette();
+  double height = 0;
+  double width = 0;
 
   static List<Color> generateRandomPalette() {
     final random = Random();
@@ -28,6 +30,19 @@ class _AnimatedColorPaletteState extends State<AnimatedColorPalette> {
     setState(() {
       currentPalette = generateRandomPalette();
     });
+    regenerateSizeS();
+  }
+
+  void regenerateSizeS() {
+    setState(() {
+      if (height == 100) {
+        height = 0;
+        width = 0;
+      } else {
+        height = 100;
+        width = 100;
+      }
+    });
   }
 
   @override
@@ -41,12 +56,21 @@ class _AnimatedColorPaletteState extends State<AnimatedColorPalette> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             for (Color color in currentPalette)
-              Container(
-                width: 100,
-                height: 100,
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.easeInExpo,
+                width: 80,
+                height: 80,
                 color: color,
                 margin: const EdgeInsets.all(8),
               ),
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.linear,
+              width: height,
+              height: width,
+              color: Colors.black,
+            ),
             ElevatedButton(
               onPressed: regeneratePalette,
               child: const Text('Generate New Palette'),
