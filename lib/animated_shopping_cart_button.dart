5255571447b8c476b53d77938269577783a2a3fb
Ansh Malik <thisisanshmalik@gmail.com>
2024-03-08 19:57:23 +0530

    Animation Complted

--- lib/animated_shopping_cart_button.dart
+++ lib/animated_shopping_cart_button.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
-class ShoppingCartButton extends StatelessWidget {
+class ShoppingCartButton extends StatefulWidget {
   const ShoppingCartButton({super.key});
 
+  @override
+  State<ShoppingCartButton> createState() => _ShoppingCartButtonState();
+}
+
+class _ShoppingCartButtonState extends State<ShoppingCartButton> {
+  bool active = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,16 +16,36 @@ class ShoppingCartButton extends StatelessWidget {
         title: const Text('Shopping Cart'),
       ),
       body: Center(
-        child: Container(
-          width: 80.0,
-          height: 60.0,
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: const Icon(
-            Icons.shopping_cart,
-            color: Colors.white,
+        child: InkWell(
+          onTap: () {
+            setState(() {
+              active = !active;
+            });
+          },
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 500),
+            width: active ? 200.0 : 80.0,
+            height: 60.0,
+            decoration: BoxDecoration(
+              color: active ? Colors.green : Colors.blue,
+              borderRadius: BorderRadius.circular(active ? 30 : 10),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(
+                  active
+                      ? Icons.add_shopping_cart_outlined
+                      : Icons.shopping_cart,
+                  color: Colors.white,
+                ),
+                if (active)
+                  const Text(
+                    "Item Added To Cart",
+                    style: TextStyle(fontWeight: FontWeight.w600),
+                  )
+              ],
+            ),
           ),
         ),
       ),
